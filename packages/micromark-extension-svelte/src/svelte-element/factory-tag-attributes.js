1350b7d995239c8c6b8a896a0d76987eb729a9d3
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-04-23 17:22:51 -0400

    tokenize attributes individually

--- packages/micromark-extension-svelte/src/svelte-element/factory-tag-attributes.js
+++ packages/micromark-extension-svelte/src/svelte-element/factory-tag-attributes.js
@@ -1,37 +1,42 @@
-/** @import {State, Code, Effects} from 'micromark-util-types'; */
+/** @import {State, Code, Effects, Tokenizer} from 'micromark-util-types'; */
 
+import { types } from 'common/constants';
 import { assert } from 'common/utils';
 import { factorySpace } from 'micromark-factory-space';
 import {
   markdownLineEnding,
   markdownLineEndingOrSpace,
+  markdownSpace,
 } from 'micromark-util-character';
 import { codes, types as coreTypes } from 'micromark-util-symbol';
 import { factoryPlainExpression } from '../factory-plain-expression.js';
 
 /**
- * Creates a state machine for parsing tag content (attributes, etc.) until
- * ending '>'. Handles whitespace, quotes, and Svelte expressions {...}.
- *
  * @param {Effects} effects
- * @param {(
- *   code: typeof codes.greaterThan | typeof codes.slash,
- * ) => State | undefined} ok
- * @param {State} nok State.
+ * @param {State} ok
+ * @param {State} nok
  */
-export function factoryTagAttributes(effects, ok, nok) {
-  return content;
+function factoryTagAttribute(effects, ok, nok) {
+  return start;
 
   /** @type {State} */
-  function content(code) {
+  function start(code) {
+    effects.enter(types.svelteElementTagAttribute);
+    return attribute(code);
+  }
+
+  /** @type {State} */
+  function attribute(code) {
     if (code === codes.slash || code === codes.greaterThan) {
+      effects.exit(types.svelteElementTagAttribute);
       return ok(code);
     }
     if (code === codes.eof) {
-      return nok;
+      return nok(code);
     }
     if (markdownLineEndingOrSpace(code)) {
-      return factorySpace(effects, content, coreTypes.whitespace)(code);
+      effects.exit(types.svelteElementTagAttribute);
+      return ok(code);
     }
     if (code === codes.quotationMark || code === codes.apostrophe) {
       return contentQuoted(code);
@@ -41,14 +46,13 @@ export function factoryTagAttributes(effects, ok, nok) {
       return factoryPlainExpression(effects, contentBracedEnd, nok);
     }
     effects.consume(code);
-    return content;
   }
 
   /** @param {Code} brace */
   function contentBracedEnd(brace) {
     assert(brace === codes.rightCurlyBrace, 'expected "}"');
     effects.consume(brace);
-    return content;
+    return attribute;
   }
 
   /**
@@ -75,7 +79,7 @@ export function factoryTagAttributes(effects, ok, nok) {
     function insideQuote(code) {
       if (code === quote) {
         effects.consume(code);
-        return content;
+        return attribute;
       }
       if (code === codes.eof || markdownLineEnding(code)) {
         return nok;
@@ -86,5 +90,37 @@ export function factoryTagAttributes(effects, ok, nok) {
       effects.consume(code);
       return insideQuote;
     }
+  }
+}
+
+/**
+ * Creates a state machine for parsing tag content (attributes, etc.) until
+ * ending '>'. Handles whitespace, quotes, and Svelte expressions {...}.
+ *
+ * @param {Effects} effects
+ * @param {(
+ *   code: typeof codes.greaterThan | typeof codes.slash,
+ * ) => State | undefined} ok
+ * @param {State} nok State.
+ */
+export function factoryTagAttributes(effects, ok, nok) {
+  return content;
+
+  /** @type {State} */
+  function content(code) {
+    if (code === codes.slash || code === codes.greaterThan) {
+      return ok(code);
+    }
+    if (code === codes.eof) {
+      return nok;
+    }
+    if (markdownLineEnding(code)) {
+      effects.consume(code);
+      return content;
+    }
+    if (markdownSpace(code)) {
+      return factorySpace(effects, content, coreTypes.whitespace)(code);
+    }
+    return factoryTagAttribute(effects, content, nok);
   }
 }
